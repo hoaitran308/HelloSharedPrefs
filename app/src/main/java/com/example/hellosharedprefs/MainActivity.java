496a9e621863d6ff12931689c973b5150a86ce61
hoaitran308 <tranvanhoai.tt4@gmail.com>
2022-03-06 16:51:31 +0700

    init project

---
+++ app/src/main/java/com/example/hellosharedprefs/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.hellosharedprefs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final String HELLO_SHARED_PREFS = "HELLO_SHARED_PREFS";
+    private static final String BLACK_KEY = "BLACK_KEY";
+    private static final String RED_KEY = "RED_KEY";
+    private static final String BLUE_KEY = "BLUE_KEY";
+    private static final String GREEN_KEY = "GREEN_KEY";
+    private SharedPreferences sharedPref;
+    private TextView number;
+    private String currentColorKey = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        onInit();
+    }
+
+    private void onInit() {
+        sharedPref = getSharedPreferences(HELLO_SHARED_PREFS, MODE_PRIVATE);
+
+        number = findViewById(R.id.number);
+
+        TextView btnBlack = findViewById(R.id.btnBlack);
+        TextView btnRed = findViewById(R.id.btnRed);
+        TextView btnBlue = findViewById(R.id.btnBlue);
+        TextView btnGreen = findViewById(R.id.btnGreen);
+
+        btnBlack.setOnClickListener(view -> onBtnColorClick(BLACK_KEY));
+        btnRed.setOnClickListener(view -> onBtnColorClick(RED_KEY));
+        btnBlue.setOnClickListener(view -> onBtnColorClick(BLUE_KEY));
+        btnGreen.setOnClickListener(view -> onBtnColorClick(GREEN_KEY));
+
+        TextView btnCount = findViewById(R.id.btnCount);
+        btnCount.setOnClickListener(view -> onBtnCountClick());
+
+        TextView btnClear = findViewById(R.id.btnClear);
+        btnClear.setOnClickListener(view -> onBtnClearClick());
+    }
+
+    private void onBtnClearClick() {
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.clear();
+        editor.apply();
+        currentColorKey = "";
+        number.setText(getResources().getString(R.string.clear_notify));
+    }
+
+    private void onBtnCountClick() {
+        if (currentColorKey.isEmpty()) {
+            return;
+        }
+
+        int newValue = Integer.parseInt(number.getText().toString()) + 1;
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(currentColorKey, newValue);
+        editor.apply();
+
+        number.setText(String.valueOf(newValue));
+    }
+
+    private void onBtnColorClick(String COLOR_KEY) {
+        currentColorKey = COLOR_KEY;
+        int numberInSharedPref = sharedPref.getInt(COLOR_KEY, 0);
+        number.setText(String.valueOf(numberInSharedPref));
+    }
+}
